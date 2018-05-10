4a364f1caf7a4f8271760e1a8a15b76e23aad853
Zack Pollard <zackpollard@ymail.com>
2018-05-10 21:02:15 +0100

    Add lock status polling

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
 "use strict";
 
-var app = require("express")();
-var swaggerTools = require("swagger-tools");
-var YAML = require("yamljs");
+const app = require("express")();
+const swaggerTools = require("swagger-tools");
+const YAML = require("yamljs");
+const request = require('superagent');
+const fs = require('fs');
 
-var mongoose = require('mongoose');
+const rpio = require("rpio");
+rpio.open(12, rpio.OUTPUT, rpio.LOW);
+rpio.open(16, rpio.OUTPUT, rpio.LOW);
+
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-var dbHost = "127.0.0.1";
-var dbPort = "27017";
-var dbName = "smartlockcore";
+const dbHost = "127.0.0.1";
+const dbPort = "27017";
+const dbName = "smartlockcore";
 
 mongoose.connect('mongodb://' + dbHost + ':' + dbPort + '/' + dbName);
-var models = require('./api/models/models');
+const models = require('./api/models/models');
+const Settings = mongoose.model('Settings');
 
-var swaggerConfig = YAML.load("./api/swagger/swagger.yaml");
+const pollServerForUpdates = function() {
+    Settings.findOne({}, function(err, task) {
+        if(err) {
+            rpio.write(12, rpio.LOW);
+            rpio.write(16, rpio.LOW);
+            console.debug("MongoDB error");
+            setTimeout(pollServerForUpdates, 0);
+        } else {
+            request
+                .get('https://smartlockapp.zackpollard.pro/api/v1/device')
+                .set('Accept', 'application/json')
+                .set('Authorization', 'Bearer ' + task.jwttoken)
+                .end((err, response) => {
+                    if(err || !response || response.status !== 200) {
+                        rpio.write(12, rpio.LOW);
+                        rpio.write(16, rpio.LOW);
+                        console.debug("Response error");
+                    } else {
+                        console.log(response.body);
+                        if(response.body.locked) {
+                            rpio.write(12, rpio.HIGH);
+                            rpio.write(16, rpio.LOW);
+                            console.debug("Response: Locked");
+                        } else {
+                            rpio.write(16, rpio.HIGH);
+                            rpio.write(12, rpio.LOW);
+                            console.debug("Response: Unlocked");
+                        }
+                    }
+                    setTimeout(pollServerForUpdates, 0);
+                });
+        }
+    });
+};
+
+pollServerForUpdates();
+
+const swaggerConfig = YAML.load("./api/swagger/swagger.yaml");
 
 swaggerTools.initializeMiddleware(swaggerConfig, function (middleware) {
     //Serves the Swagger UI on /docs
     app.use(middleware.swaggerMetadata());
 
-    var routerConfig = {
+    const routerConfig = {
         controllers: "./api/controllers",
         useStubs: false
     };
